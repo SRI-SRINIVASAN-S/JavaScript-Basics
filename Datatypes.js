bb7f5d86d9c3ff1ed792c4535b1532a4ad120c31
SRI-SRINIVASAN-S <srini624618@gmail.com>
2022-06-22 11:54:42 +0530

    All about data types

---
+++ Datatypes.js
@@ -0,0 +1,57 @@
+// There are 2 types in data types in JS  1.primitive 2.Non primitive
+//1. .primitive data types (primitive data types are a set of basic data types from which all other data types are constructed)
+
+//String
+const data1 ='srini' //single quotes
+const data2 ="srini" //double quotes
+console.log(typeof(data1))
+console.log(typeof(data2))
+
+//Number 
+const data3 =123454 
+const data4 =125.56
+console.log(typeof(data3))
+console.log(typeof(data4))
+
+//Boolean 
+const data5 =true //1
+const data6 =false //0
+console.log(typeof(data5))
+console.log(typeof(data6))
+
+//undefined         //A variable without a value is undefined 
+let data7 ;
+console.log(typeof(data7))
+let data8 ;
+console.log(typeof(data8))
+
+//Non primitive data types
+
+// Array
+let data9 ;
+data9 =['john',28],['rock',30],['bigshow',40] //create array by [] & newArray(method)
+let data10;
+data10 =new Array(['sai',10],['anriya',25])
+console.log(typeof(data9))
+console.log(typeof(data10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
